18e186f04f537c30b51da85a6c5543d456420b4b
Joyce Ebruphiyo Etata <164307921+joyceetata2@users.noreply.github.com>
2024-04-16 06:05:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+print("Hello World!")
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+print("Hello World!\nWelcome to Python class Day 1")
+print("Hello " + ""
+      "Angela")
+print("Hello Joyce!! Welcome to Python Class")
+print("Hello " + 
+      "Joyce Welcome to Day 1 of the python class!")
